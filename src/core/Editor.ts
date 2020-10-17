3698d5b7d820da60478b9a38566c44afa10538e3
luochao <luochao@yy.com>
2020-10-17 12:38:00 +0800

    feat: 完善富文本框的功能

--- src/core/Editor.ts
+++ src/core/Editor.ts
@@ -19,6 +19,7 @@ class Editor {
   id: string
   contentEditable: ContentEditable
   container?: HTMLElement
+  range?: Range | null
 
   constructor(selector: string | HTMLElement, options?: EditorOptions) {
     this.selector = selector
@@ -42,6 +43,10 @@ class Editor {
     })
 
     this.contentEditable = new ContentEditable(this.value)
+
+    this.contentEditable.element.addEventListener('blur', () => {
+      this.saveRange()
+    })
 
     this.init()
   }
@@ -72,10 +77,55 @@ class Editor {
   }
 
   setTextStyle(type: string) {
+    // 添加效果之前，先设置range
+    this.setRange()
+
     if (type === 'heading') {
       document.execCommand('formatBlock', false, 'h1')
     } else {
       document.execCommand(type)
+    }
+  }
+  // 保存当前range
+  saveRange() {
+    const selection = window.getSelection()
+    if (selection == null || selection.rangeCount === 0) {
+      return
+    }
+    const content = this.contentEditable.element
+    for (let i = 0; i < selection.rangeCount; i++) {
+      // 从selection中获取第一个Range对象
+      const range = selection.getRangeAt(0)
+      let start = range.startContainer
+      let end = range.endContainer
+      // 兼容IE11 node.contains(textNode) 返回false的bug
+      start = start.nodeType === Node.TEXT_NODE ? start.parentNode! : start
+      end = end.nodeType === Node.TEXT_NODE ? end.parentNode! : end
+      if (content.contains(start) && content.contains(end)) {
+        this.range = range
+        break
+      }
+    }
+  }
+
+  setRange() {
+    const selection = window.getSelection()
+    if (selection == null) return
+    // 清除当前range
+    selection.removeAllRanges()
+
+    if (this.range != null) {
+      selection.addRange(this.range)
+    } else {
+      // 如果没有，创建一个新的range存起来
+      const content = this.contentEditable.element
+      const row = document.createElement('br')
+      const range = document.createRange()
+      content.appendChild(row)
+      range.setStart(row, 0)
+      range.setEnd(row, 0)
+      selection.addRange(range)
+      this.range = range
     }
   }
 
