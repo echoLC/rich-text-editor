b426e4972f538cf70c9a5d6dc592a8d504fb0bb2
luochao <luochao@yy.com>
2020-11-03 21:09:22 +0800

    fix: 完善cypress e2e测试

--- cypress/integration/editor_spec.js
+++ cypress/integration/editor_spec.js
@@ -1,43 +1,58 @@
 describe('Editor init', () => {
   beforeEach(() => {
     // per test suit reload page
-    // window.location.reload()
     cy.reload()
   })
 
   it('it should can init basic editor by new Editor instance pass dom', () => {
+    // 访问首页
     cy.visit('/')
 
-    cy.get('#editor').find('.editor-toolbar').first()
-    cy.get('#editor').find('.editor-toolbar').children().should('have.length', 3)
-    cy.get('#editor').find('.editor-content').last()
+    cy.get('#editor')
+      .as('Editor') // 给editor元素加个别名，方便后面调用
+      .should('have.class', 'editor-container') // 断言是否包括editor-container类名
 
+    // 使用前面的别名获取编辑器，并断言editor-toolbar是其第一个子元素
+    cy.get('@Editor').find('.editor-toolbar').first()
+
+    // 查找到toolbar元素，并且断言其有3个子元素
+    cy.get('.editor-toolbar').children().should('have.length', 3)
+
+    // 使用前面的别名获取编辑器，并断言editor-toolbar是其最后一个子元素
+    cy.get('@Editor').find('.editor-content').last()
+
+    // 断言 editor-content 元素包含 test 文本
     cy.get('.editor-content').should('contain', 'test')
   })
 
   it('it should click h button make text change to h1 title', () => {
-    cy.get('#editor')
-      .find('.editor-content')
+    // 找到editor-content，并且使其获得焦点
+    cy.get('.editor-content')
       .focus()
-      .then(contentEl => {
-        cy.get('#editor').find('.editor-toolbar').children().first().click()
+      .then(() => {
+        // 获得焦点后，点击H按钮
+        cy.get('.editor-toolbar').children().first().click()
 
-        cy.get('#editor').find('.editor-content').get('h1').should('not.to.be', null)
+        // 断言编辑器里面的内容添加标题h1，并且内容是 test
+        cy.get('.editor-content').get('h1').contains('test')
       })
   })
 
   it('it should click b button make text change to bold style', () => {
-    cy.get('#editor')
-      .find('.editor-content')
+    // 找到editor-content，并且使其获得焦点，点击后
+    cy.get('.editor-content')
       .focus()
       .click()
       .then(contentEl => {
+        // 执行选中内容命令
         cy.get(contentEl)
           .type('{selectall}')
           .then(() => {
-            cy.get('#editor').find('.editor-toolbar').children().eq(1).click()
+            // 点击toolbar bold按钮
+            cy.get('.editor-toolbar').children().eq(1).click()
 
-            cy.get('#editor').find('.editor-content').get('b')
+            // 断言编辑器里面的内容添加 b标签而变成加粗样式，并且内容是 test
+            cy.get('.editor-content').get('b').contains('test')
           })
       })
   })
